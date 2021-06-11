8f48baf0b00fe4ab26e1b6d060d2a03f5b3d201a
Lucas de Morais <lucasmoraismt@gmail.com>
2021-06-11 02:33:42 -0300

    get /participants done

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/participants", (req, res) => {
+app.post("http://localhost:4000/participants", (req, res) => {
   const body = req.body;
   const usedName = users.find((p) => p.name === body.name);
   if (body.name.trim().length === 0) {
@@ -33,6 +33,10 @@ app.post("/participants", (req, res) => {
     messages.push(newMessage);
     res.status(200);
   }
+});
+
+app.get("http://localhost:4000/participants", (req, res) => {
+  res.send(users);
 });
 
 app.listen(4000);
